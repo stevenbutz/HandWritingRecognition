3acf9eb7a42ffc798dcd886b0aeba1947ee093ef
Steven Butz <stephbutzz@gmail.com>
2023-11-26 01:11:54 -0500

    Code

--- feedforeward.py
+++ feedforeward.py
@@ -67,4 +67,18 @@ model.eval()
 correct = 0
 total = 0
 
+# Disabling gradient calculation
+with torch.no_grad():
+    for data, target in test_loader:
+        # Forward pass
+        output = model(data)
+
+        # Get the index of the max log-probability
+        _, predicted = torch.max(output.data, 1)
+        total += target.size(0)
+        correct += (predicted == target).sum().item()
+
+# Calculate and print the accuracy
+accuracy = 100 * correct / total
+print(f'Accuracy of the network on the test images: {accuracy:.2f}%')
 
